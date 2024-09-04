a8e46be392533f9cb205020d90de8e0fc3a20240
Daniel Hernandez <143033040+dh21704@users.noreply.github.com>
2024-09-03 21:00:40 -0700

    Create InsuranceInfo.java

---
+++ InsuranceInfo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.classexercise3;
+
+
+public class InsuranceInfo extends Vehicle{
+    
+    InsuranceInfo i = new InsuranceInfo();
+    
+    
+    public void Insurance()
+    {
+        System.out.println("Enter the insurance for ");//+ i.model);
+        brand = scan.nextLine();
+    }
+}
