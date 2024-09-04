0358d17c9eea16c860c6f07615ed34daabb05a66
Daniel Hernandez <143033040+dh21704@users.noreply.github.com>
2024-09-03 21:00:54 -0700

    Create Manager.java

---
+++ Manager.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.classexercise3;
+
+/**
+ *
+ * @author danielhernandez
+ */
+public class Manager extends Company{
+    
+    String ManagerID, ManagerName;
+    
+    
+    
+    
+}
