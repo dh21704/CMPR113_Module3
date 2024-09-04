5abd370829889544009fe6e463c11e0ec04a9a5b
Daniel Hernandez <143033040+dh21704@users.noreply.github.com>
2024-09-03 20:59:22 -0700

    Create ClassExercise3.java

---
+++ ClassExercise3.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.classexercise3;
+
+
+/**
+ *
+ * @author danielhernandez
+ * 
+ */
+class Employee
+{
+    int salary = 90000, age;
+    String EmployeeName = "JasonSim", Address,
+            City, State, Zipcode;
+    
+    
+    
+          
+}
+
+
+public class ClassExercise3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
