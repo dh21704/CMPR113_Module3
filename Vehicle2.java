503c45138170e1e56f2e143200b05c57808a782e
Daniel Hernandez <143033040+dh21704@users.noreply.github.com>
2024-09-08 20:17:06 -0700

    Create Vehicle2.java

---
+++ Vehicle2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.classexercise3;
+
+/**
+ *
+ * @author danielhernandez
+ */
+class Vehicle2 {
+    protected String brand2 = "Ford";
+    protected String model2 = "Mustang";
+    
+    public String TheCar2()
+    {
+        return "The car you choose is";
+    }
+}
