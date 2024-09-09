cccf996fe45538b2b8abad4d41432827d4acaafc
Daniel Hernandez <143033040+dh21704@users.noreply.github.com>
2024-09-08 20:17:19 -0700

    Create Car2.java

---
+++ Car2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.classexercise3;
+
+/**
+ *
+ * @author danielhernandez
+ */
+class Car2 extends Vehicle2{
+    public String getMyCar2()
+    {    
+        return (TheCar2() + " " + brand2 + " " + model2);
+    }
+}
